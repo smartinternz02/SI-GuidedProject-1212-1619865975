75b392ab8674977da69853af3bd14fe5aa2d4132
anudeepchennoju <83569431+anudeepchennoju@users.noreply.github.com>
2021-05-20 22:47:29 +0530

    Add files via upload

---
+++ assignment_3_fresh.ino
@@ -0,0 +1,108 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+#define DHTPIN 14     // Digital pin connected to the DHT sensor
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+
+  dht.begin();
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+}
+
+void loop() {
+  delay(5000);
+//read ldr values
+int ldrval=analogRead(15);
+Serial.print("the LDR value is:");
+Serial.println(ldrval);
+delay(1000);
+   
+  //read temperature and humidity
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+  }
+  // clear display
+  display.clearDisplay();
+  
+  // display temperature
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("Temperature: ");
+  display.setTextSize(2);
+  display.setCursor(0,10);
+  display.print(t);
+  display.print(" ");
+  display.setTextSize(1);
+  display.cp437(true);
+  display.write(167);
+  display.setTextSize(2);
+  display.print("C");
+  
+  // display humidity
+  display.setTextSize(1);
+  display.setCursor(0,25);
+  display.print("Humidity: ");
+  display.setTextSize(2);
+  display.setCursor(0,45);
+  display.print(h);
+  display.print(" %"); 
+  
+  display.display(); 
+
+  //display ldr values
+  display.setTextSize(1);
+  display.setCursor(0,55);
+  display.print("ldrvalue: ");
+  display.setTextSize(2);
+  display.setCursor(0,75);
+  display.print(ldrval);
+If(ldrval<700)
+{
+digitalWrite(2,HIGH);
+display.println(“LIGHT is ON”);
+}
+Else
+
+{
+digitalWrite(2,LOW);
+display.println(“ LIGHT is OFF”);
+}
+If(f>28&&h>40)
+{
+digitalWrite(13,HIGH);
+display.println(“FAN is ON”);
+
+}
+else{
+digitalWrite(13,LOW);
+display.println(“FAN is OFF”);
+}
+}
+
+//done by anudeep
